b300c872725f9b05ca1ff3a7999e2c53bfc265a9
Dhananjay Mehta <dhananjaymehta@ksu.edu>
2026-04-08 23:16:37 -0700

    Add: evaluate-reverse-polish-notation - submission-11

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        return self.recursion_dfs(tokens)
+
+    def recursion_dfs(self, tokens):
+        token = tokens.pop()
+        if token not in '+-*/':
+            return int(token)
+        right = self.recursion_dfs(tokens)
+        left = self.recursion_dfs(tokens)        
+        
+        if token == '+':
+            return left + right
+        elif token == '-':
+            return left - right
+        elif token == '*':
+            return left * right
+        elif token == '/':
+            return int(left / right)
