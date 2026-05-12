8d6bb14e17e70d4789fb8139e4d75cacf29c5e03
Dhananjay Mehta <dhananjaymehta@ksu.edu>
2026-05-11 20:12:42 -0700

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,49 @@
+class Solution:
+    def __init__(self):
+        self.max_heap=[]
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # initalize the indexes for windows
+        # set a max heap
+        # insert the window into max heap as initialization 
+        # move the window one element at a time l+1, r+1 -> 
+        # before moving identify the  max element: if max element in valid range then pop else remove out of range max 
+        # check the new element and insert it
+        if len(nums)<1 or k<1:
+            return []
+        
+        l, r = 0, 0
+        res = []
+        
+        import heapq
+        while r<len(nums):
+            if (r-l+1)<k:
+                heapq.heappush(self.max_heap, (-nums[r], r))
+                r+=1
+            else:
+                # add next item in the heap
+                heapq.heappush(self.max_heap, (-nums[r], r))
+                # get max item of the heap
+                res.append(self.get_max_item(l, r))
+                l+=1
+                r+=1                
+            # print(l, r)
+        return res
+
+    def get_max_item(self, l, r):
+        # check if the top of heap is within the range
+        # if not: while top not b/w l and r -> pop top
+        # else: get the top item        
+        val, ind = self.max_heap[0]
+        cnt=0
+        # print(self.max_heap)
+        # print(l, r, -val, ind)
+        while not (l <= ind <=r):            
+            heapq.heappop(self.max_heap)
+            val, ind = self.max_heap[0]
+            cnt+=1
+            if cnt>100:
+                break        
+        val, ind = self.max_heap[0]
+        
+        return -val
+        
